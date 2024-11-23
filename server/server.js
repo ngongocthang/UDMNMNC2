a76b19b6ecfe8241cd1f47abfa99edc9c7e4480d
ngongocthang <ngongocthang18082004@gmail.com>
2024-11-23 12:54:57 +0700

    change vercel

--- server/server.js
+++ server/server.js
@@ -14,13 +14,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-// Cấu hình CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL, // Lấy từ biến môi trường
+    origin: ["https://udmnmnc-2-fe.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // Cho phép cookie nếu cần
+    credentials: true,
   })
 );
 
